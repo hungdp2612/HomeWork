fdab8476fedc43de2d521c3eaae4941538a3e6c7
hung2612 <hungdp2612@gmail.com>
2019-09-29 14:55:42 +0700

    Excercise4

---
+++ IsPrime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrime(8));
+        }
+        static bool IsPrime(int n)
+        {
+            if(n <= 1)
+                return false;
+            for (int i = 2; i < n; i++) if (n % i == 0) 
+            {
+                return false;
+            } 
+            return true;
+        }
+    }
+}
