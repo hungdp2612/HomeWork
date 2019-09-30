6e920a418085a6787c0acb05332ce786bd4a02fc
hung2612 <hungdp2612@gmail.com>
2019-09-30 21:00:00 +0700

    https://github.com/hungdp2612/HomeWork.git

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsEven(3));
+        }
+        static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Square/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Square(4));
+        }
+        static int Square(int n)
+        {
+            return n * n;
+        }
+    }
+}
